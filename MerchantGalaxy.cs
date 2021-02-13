7260993ea1e62a68e6bd13a69311c2c03e70206d
Shivam Bhargava <38013354+07shivam@users.noreply.github.com>
2021-02-13 21:48:24 +0530

    Add files via upload

---
+++ MerchantGalaxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantGalaxyProblem
+{
+    public class MerchantGalaxy
+    {
+        private static void Main(string[] args)
+        {
+            string[] input =
+            {
+                "glob is I","prok is V","pish is X","tegj is L",
+                 "glob glob Silver is 34 Credits","glob prok Gold is 57800 Credits","pish pish Iron is 3910 Credits",
+                "how much is pish tegj glob glob ?",
+                "how many Credits is glob prok Silver ?",
+                "how many Credits is glob prok Gold ?",
+                "how many Credits is glob prok Iron ?",
+                "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?"
+            };
+
+            var parser = new Parse();
+            parser.getConditions(input);
+            parser.getCreditsCondition(input);
+            parser.getQuestion(input);
+            List<string> ans = parser.SolveQuestion();
+            foreach (var f in ans)
+            {
+                Console.WriteLine(f);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RomanNumberValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantGalaxyProblem
+{
+    public class RomanNumberValidator
+    {
+        private string[] InvalidRomanNumberPattern = new string[]
+       {
+            "IIII","XXXX","CCCC","MMMM",
+            "DD","LL","VV",
+            "IL","IC","ID","IM",
+            "XD","XM",
+            "VX","VL","VC","VD","VM",
+            "LC","LD","LM",
+            "DM"
+       };
+
+        public bool Validate(string numeral)
+        {
+            foreach (var invalidPattern in InvalidRomanNumberPattern)
+            {
+                if (numeral.Contains(invalidPattern))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
